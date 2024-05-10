c0dee455abe467376d78a77884ccf69de5662132
Sai Htet Myat Htut <Saihankli5@gmail.com>
2024-05-10 21:02:00 +0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<div class="w-screen h-screen">
+    <section
+        class="w-full h-full relative bg-cover bg-[url('images/scooter-background.jpg')]"
+    >
+        <!-- <div
+            class="w-full h-full absolute left-0 top-0 bg-black opacity-20"
+        ></div> -->
+        <div class="w-full h-full flex flex-col px-[100px]">
+            <div class="h-[80px] flex flex-row">
+                <div class="w-[80px] h-[80px] bg-red-400">
+
+                </div>
+                <div class="flex flex-row flex-grow justify-center items-center space-x-3 font-bold text-black">
+                    <button>Home</button>
+                    <button>About</button>
+                    <button>Services</button>
+                    <button>Career</button>
+                    <button>Blog</button>
+                </div>
+                <div class="w-[160px] h-[80px] flex justify-center items-center">
+                    <button
+                    class="w-32 h-12 rounded-full text-white font-bold tracking-wide hover:bg-green-700 bg-green-500"
+                    >Contact Us</button>
+                </div>
+            </div>
+            <div class="flex-grow"></div>
+            <div class=" text-6xl text-black font-bold mb-5">
+                Scooter All You Need
+            </div>
+            <div class=" w-[600px] text-black mb-5">
+                A scooter (motor scooter) is a motorcycle with an underbone or
+                step-through frame, a seat, a transmission that shifts without
+                the operator having to operate a clutch lever, a platform for
+                the rider's feet, and with a method of operation that emphasizes
+                comfort and fuel economy.
+            </div>
+            <div class="">
+                <button
+                    class="w-32 h-12 rounded-full text-white font-bold tracking-wide hover:bg-green-700 bg-green-500"
+                    >Buy Now</button>
+            </div>
+            <div class="h-[120px]"></div>
+        </div>
+    </section>
+</div>
